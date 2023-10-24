24b2e2a70c9d6aeb3bb364ef7851fdfbea396b11
Shishir Halder <s.halder0108@gmail.com>
2023-10-24 13:09:27 +0600

    feat: add cors

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
