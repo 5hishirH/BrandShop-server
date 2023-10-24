e7da0f62aabe4321f1bd1e985bebd84085730e3a
Shishir Halder <s.halder0108@gmail.com>
2023-10-24 06:28:09 +0600

    feat: add dynamic routes for each brands

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,13 @@ async function run() {
     app.get("/brands", async (req, res) => {
       const cursor = brandCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/brands/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await brandCollection.findOne(query);
       res.send(result);
     });
   } finally {
